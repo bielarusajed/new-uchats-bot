d967c52aeeeffe2c76d9ec017e4c4f71bbceebb8
Aliaksiej Razumaŭ <belarusaed@gmail.com>
2025-01-05 18:44:08 +0100

    reports

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,9 +1,10 @@
 import { Composer } from 'grammy';
 import { mute } from './mute';
+import { report } from './report';
 import { rules } from './rules';
 
 const commands = new Composer();
 
-commands.use(rules).use(mute);
+commands.use(rules).use(mute).use(report);
 
 export { commands };
---
+++ src/commands/report.ts
@@ -0,0 +1,34 @@
+import { Composer, type Context } from 'grammy';
+
+const report = new Composer();
+
+async function reportAdmin(ctx: Context) {
+  if (!ctx.chat) return;
+  const admins = await ctx.api.getChatAdministrators(ctx.chat.id);
+  for (const admin of admins) {
+    try {
+      await ctx.api.sendMessage(
+        admin.user.id,
+        `https://t.me/c/${Math.abs(ctx.chat.id) > 10 ** 12 ? Math.abs(ctx.chat.id) - 10 ** 12 : Math.abs(ctx.chat.id)}/${ctx.message?.message_id}`,
+      );
+    } catch {}
+  }
+  ctx.reply('Адрапартавана!');
+}
+
+report.command(['report', 'admin'], async ctx => {
+  await reportAdmin(ctx);
+});
+
+report.on('::mention', async ctx => {
+  const isAdmin =
+    (ctx.message?.entities
+      ?.map(entity => ctx.message?.text?.slice(entity.offset, entity.offset + entity.length).toLocaleLowerCase())
+      .filter(mention => mention === '@admin').length ?? -1) > 0;
+
+  if (!isAdmin) return;
+
+  await reportAdmin(ctx);
+});
+
+export { report };
