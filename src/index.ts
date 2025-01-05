f62a5cc67ef5205275a507b3ff1b8ce9276ad3ef
Aliaksiej Razumaŭ <belarusaed@gmail.com>
2025-01-05 15:18:52 +0100

    Removed test handlers

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,4 @@ const bot = new Bot(Bun.env.BOT_TOKEN);
 
 bot.use(commands);
 
-bot.command('start', ctx => {
-  return ctx.reply('Welcome! Up and running.');
-});
-
-bot.on('message', ctx => {
-  console.log(ctx.update.message.chat.id);
-  return ctx.reply('Got another message!');
-});
-
 bot.start();
