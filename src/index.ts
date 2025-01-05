181a6c43331c98e1584acc2a63ea4bcafc1e0e14
Aliaksiej Razumaŭ <belarusaed@gmail.com>
2025-01-05 18:06:13 +0100

    Basic error handling

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-import { Bot } from 'grammy';
+import { Bot, GrammyError, HttpError } from 'grammy';
 import { commands } from './commands';
 
 const bot = new Bot(Bun.env.BOT_TOKEN);
 
 bot.use(commands);
+
+bot.catch(err => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error('Error in request:', e.description);
+  } else if (e instanceof HttpError) {
+    console.error('Could not contact Telegram:', e);
+  } else {
+    console.error('Unknown error:', e);
+  }
+});
 
 bot.start();
